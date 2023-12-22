6f1aa2cb0ee7686174ff2f8c7b78e374b8676adf
NicolasArroyo <nicolasmach07@gmail.com>
2023-12-22 17:42:22 -0500

    New API for user queries

---
+++ api.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from spimi_indexer import initialize_index, compute_vector_score
+import json
+
+initialize_index()
+
+
+class Item(BaseModel):
+    query: str
+    k: int
+
+
+with open("./filenames_dict.json", "r", encoding='utf-8') as file:
+    filenames_dict = json.load(file)
+
+app = FastAPI()
+
+
+@app.post("/api/index/")
+def post_api(item: Item):
+    results = compute_vector_score(item.query, "./merged_index_tfidf.txt", item.k)
+
+    response = {}
+    for doc_id, score in results:
+        response[filenames_dict[f"doc{doc_id}"]] = score
+
+    return response
