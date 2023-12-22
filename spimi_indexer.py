6e754e316da1692981e4f66c46a7a5f8c937f2e7
NicolasArroyo <nicolasmach07@gmail.com>
2023-12-21 22:25:03 -0500

    Dictionary size is calculated recursively and now stores term frequency alongside file ID

--- spimi_indexer.py
+++ spimi_indexer.py
@@ -3,7 +3,32 @@ import os
 import json
 from text_processing_pipeline import process_files_txt
 
-BLOCK_SIZE = 4096
+BLOCK_SIZE = 40000
+
+
+# Get total size of dictionary recursively as sys.getsizeof(dict) might give incorrect results
+def get_total_size(obj, seen=None):
+    size = sys.getsizeof(obj)
+
+    if seen is None:
+        seen = set()
+
+    obj_id = id(obj)
+
+    if obj_id in seen:
+        return 0
+
+    seen.add(obj_id)
+
+    if isinstance(obj, dict):
+        size += sum([get_total_size(v, seen) for v in obj.values()])
+        size += sum([get_total_size(k, seen) for k in obj.keys()])
+    elif hasattr(obj, '__dict__'):
+        size += get_total_size(obj.__dict__, seen)
+    elif hasattr(obj, '__iter__') and not isinstance(obj, (str, bytes, bytearray)):
+        size += sum([get_total_size(i, seen) for i in obj])
+
+    return size
 
 
 def save_block(block_name, block_dict):
@@ -13,31 +38,35 @@ def save_block(block_name, block_dict):
 def spimi_invert(token_stream):
     block_counter = 0
     dictionary = {}
+    dictionary_checkpoint = {}
 
     for token in token_stream:
-        dictionary_checkpoint = dictionary
+        dictionary_checkpoint = dictionary.copy()
 
         token_word = token[0]
         token_file_id = token[1]
 
         if token_word not in dictionary:
-            dictionary[token_word] = [token_file_id]
+            dictionary[token_word] = [[token_file_id, 1]]
+        elif dictionary[token_word][-1][0] == token_file_id:
+            dictionary[token_word][-1][1] += 1
+        elif dictionary[token_word][-1][0] != token_file_id:
+            dictionary[token_word].append([token_file_id, 1])
 
-        elif dictionary[token_word][-1] != token_file_id:
-            dictionary[token_word].append(token_file_id)
+        if get_total_size(dictionary) > BLOCK_SIZE:
+            sorted_dictionary = dict(sorted(dictionary_checkpoint.items()))
+            save_block(f"block{block_counter}", sorted_dictionary)
 
-        # TODO: When returning to checkpoint one word is lost
+            print(block_counter, get_total_size(sorted_dictionary), sorted_dictionary)
 
-        if sys.getsizeof(dictionary) > BLOCK_SIZE:
-            dictionary = dict(sorted(dictionary.items()))
-            print(dictionary)
-
-            dictionary = dictionary_checkpoint
-            save_block(f"block{block_counter}", dictionary)
-
-            dictionary = {}
+            dictionary = {token_word: [[token_file_id, 1]]}
             dictionary_checkpoint = {}
             block_counter += 1
+
+    if dictionary:
+        sorted_dictionary = dict(sorted(dictionary.items()))
+        save_block(f"block{block_counter}", sorted_dictionary)
+        print(block_counter, get_total_size(sorted_dictionary), sorted_dictionary)
 
 
 if __name__ == "__main__":
